3f2a7fda1e729cc141278bde4f53fba826748200
billy <info.runningfrogs@gmail.com>
2024-03-15 17:26:26 +0100

    Intermediate

--- config/paths.py
+++ config/paths.py
@@ -2,13 +2,16 @@
 path_unprepared_training_data = 'general/datasets/training_data/training_data_unprepared.csv'
 path_test_data_basic = 'general/datasets/test_data/test_data_basic.csv'
 path_test_data_expanded = 'general/datasets/test_data/test_data_expanded.csv'
+path_prepared_training_data_original = 'general/datasets/training_data/training_data_prepared_original.csv'
+path_evaluation_data = 'general/datasets/training_data/evaluation_data.csv'
+path_prepare_data_original_log = 'general/datasets/log/cleanup_log.txt'
 
 ## Original model paths
 # Datasets
 path_prognosed_data_original = 'datasets/prognosed_data/prognosed_data.csv'
-path_prepare_data_original_log = 'models/original/data_preparation/log/cleanup_log.txt'
-path_prepared_training_data_original = 'models/original/datasets/training_data/training_data_prepared.csv'
-path_evaluation_data = 'models/original/datasets/training_data/evaluation_data.csv'
+
+
+
 
 # Analysis
 path_analysis_result_training_data_original = 'analysis/results/training_data/'
